e83135edc63b26d2e0556d2da4b2f05529ec15ce
Gil Solis <gilsolisuw@gmail.com>
2021-03-19 12:55:11 -0700

    moved randomization to server side

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ document.getElementById('flip').addEventListener('click', flip)
 let heads = 0
 let tails = 0
 
-function flip() {
+async function flip() {
 
-    let headsOrTails = Math.ceil(Math.random() * 2)
-    if (headsOrTails === 1) {
+    const res = await fetch('/coinflip')
+    const data = await res.json()
+    console.log(data)
+
+    if (data.value === '1') {
         heads += 1
         document.querySelector('h2').innerHTML = 'Heads'
         document.getElementById('heads').innerHTML = `Heads:${heads}`
--- server.js
+++ server.js
@@ -10,7 +10,20 @@ app.get('/', (req, res) => {
 
 })
 
+app.get('/coinflip', (req, res) => {
+    // res.sendFile(__dirname + '/js/main.js')
+    let headsOrTails = Math.ceil(Math.random() * 2)
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const objToJson = {
+        value: `${headsOrTails}`
+    }
+    res.end(JSON.stringify(objToJson))
+})
+
 app.get('/js/main.js', (req, res) => {
     res.sendFile(__dirname + '/js/main.js')
 })
 
+app.get('/css/style.css', (req, res) => {
+    res.sendFile(__dirname + '/css/style.css')
+})
