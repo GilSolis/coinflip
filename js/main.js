1c468fa38a7684e3f48eee89e7397a8fed3ef7b1
Gil Solis <gilsolisuw@gmail.com>
2021-03-23 19:46:07 -0700

    clean up

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ let tails = 0
 async function flip() {
 
     const res = await fetch('/coinflip')
+
     const data = await res.json()
     console.log(data)
-
     if (data.value === '1') {
         heads += 1
         document.querySelector('h2').innerHTML = 'Heads'
@@ -17,4 +17,5 @@ async function flip() {
         document.querySelector('h2').innerHTML = 'Tails'
         document.getElementById('tails').innerHTML = `Tails:${tails}`
     }
+
 }
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
+// const MongoClient = require('mongodb').MongoClient
 
 app.listen(5000, function () {
     console.log('listening on 5000')
 })
+// let connectionString = 'mongodb+srv://Admin2:password@123@cluster0.tgqse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// MongoClient.connect(connectionString, (err, client) => {
+//     console.log('Connected to Database')
+//     const db = client.db('coinflipDB')
+//     const quotesCollection = db.collection('coinflipResults')
+// app.get('/', (req, res) => {
+//     db.collection('coinflipDB').find.toArray()
+//         .then(results => {
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-
-})
-
+//         })
+// })
 app.get('/coinflip', (req, res) => {
     // res.sendFile(__dirname + '/js/main.js')
     let headsOrTails = Math.ceil(Math.random() * 2)
@@ -19,6 +25,26 @@ app.get('/coinflip', (req, res) => {
     }
     res.end(JSON.stringify(objToJson))
 })
+// app.put('/addResult', (res, req) => {
+//     console.log('adding one')
+//     // res.end('Success')
+// })
+// })
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+
+})
+
+// app.get('/coinflip', (req, res) => {
+//     // res.sendFile(__dirname + '/js/main.js')
+//     let headsOrTails = Math.ceil(Math.random() * 2)
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     const objToJson = {
+//         value: `${headsOrTails}`
+//     }
+//     res.end(JSON.stringify(objToJson))
+// })
 
 app.get('/js/main.js', (req, res) => {
     res.sendFile(__dirname + '/js/main.js')
@@ -26,4 +52,7 @@ app.get('/js/main.js', (req, res) => {
 
 app.get('/css/style.css', (req, res) => {
     res.sendFile(__dirname + '/css/style.css')
+})
+app.get('/img/heads.png', (req, res) => {
+    res.sendFile(__dirname + '/img/heads.png')
 })
